9276774ea2ce34d439e6d5650b18174edbf999ab
Humza Butt <72743279+UP942033@users.noreply.github.com>
2024-04-13 13:15:04 +0100

    Attempted to add persistent session

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,31 @@
 </template>
 
 <script>
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 export default {
   name: 'App',
+  data() {
+    return {
+      user: null
+    };
+  },
+  created() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        this.user = user;
+        // Optionally redirect the user or update the state
+        this.$router.push({ name: 'GroupCreation' });
+      } else {
+        // User is signed out
+        this.user = null;
+        this.$router.push({ name: 'Login' });
+      }
+    });
+  }
 };
 </script>
 
