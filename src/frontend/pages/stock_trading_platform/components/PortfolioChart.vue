bbd2ac0dd4b93d151a6452ba2c98660f8072c1ae
Humza Butt <Humzab1711@hotmail.com>
2024-06-02 00:23:46 +0100

    Exists

---
+++ src/frontend/pages/stock_trading_platform/components/PortfolioChart.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <LineChart :chartData="chartData" :options="options" />
+    </div>
+  </template>
+  
+  <script>
+  import { Line } from 'vue-chartjs';
+  import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, PointElement, LinearScale, CategoryScale } from 'chart.js';
+  
+  ChartJS.register(Title, Tooltip, Legend, LineElement, PointElement, LinearScale, CategoryScale);
+  
+  export default {
+    name: 'PortfolioChart',
+    components: {
+      LineChart: Line
+    },
+    props: {
+      chartData: {
+        type: Object,
+        required: true
+      },
+      options: {
+        type: Object,
+        required: false,
+        default: () => ({})
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .chart-container {
+    position: relative;
+    margin: auto;
+    height: 40vh;
+    width: 80vw;
+  }
+  </style>
+  
