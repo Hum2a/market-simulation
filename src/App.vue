b93e1a2ffe2d1f508beb8dc51e5e25a059c2e72b
Humza Butt <Humzab1711@hotmail.com>
2024-06-27 21:01:28 +0100

    Api stuff

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
+  <div id="app">
     <router-view />
   </div>
 </template>
 
 <script>
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { isDataFetchedForToday, saveStockDataToFirestore } from './backend/firebase/initFirebase'; // Ensure paths are correct
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { isDataFetchedForToday, saveStockDataToFirestore } from './backend/firebase/initFirebase';
+import { fetchAndSaveConversionRate } from './backend/api/ConversionRateChecker';
+import './backend/api/FinnHubAPI'; // Importing the script to ensure it runs
+import { trackUserLogin, stopTrackingUserLogin } from './backend/utils/loginTracker';
+import { trackUserLogout } from './backend/utils/logoutTracker';
 
+// Alpha Vantage API key
 const API_KEY = 'Z2G35L67NYNFFXHT';
 
 const getStockData = async (symbol) => {
@@ -27,60 +32,21 @@ export default {
     return {
       user: null,
       companies: [
-        { name: 'AbbVie', symbol: 'ABBV' },
-        { name: 'Activision Blizzard', symbol: 'ATVI' },
-        { name: 'Adobe', symbol: 'ADBE' },
         { name: 'Amazon', symbol: 'AMZN' },
-        { name: 'American Tower Corporation', symbol: 'AMT' },
         { name: 'Apple', symbol: 'AAPL' },
-        { name: 'Astra Zeneca', symbol: 'AZN' },
-        { name: 'AT&T', symbol: 'T' },
-        { name: 'Axon Enterprise', symbol: 'AXON' },
         { name: 'Barclays', symbol: 'BCS' },
-        { name: 'Berkshire Hathaway', symbol: 'BRK.B' },
-        { name: 'Blackrock', symbol: 'BLK' },
-        { name: 'Boeing', symbol: 'BA' },
-        { name: 'BP', symbol: 'BP' },
-        { name: 'BYD', symbol: 'BYDDY' },
-        { name: 'Cisco', symbol: 'CSCO' },
+        { name: 'Boeing', symbol: 'BA'},
         { name: 'Coca-Cola', symbol: 'KO' },
-        { name: 'Comcast', symbol: 'CMCSA' },
-        { name: 'Costco', symbol: 'COST' },
-        { name: 'Curries', symbol: 'DC.L' },
         { name: 'Disney', symbol: 'DIS' },
-        { name: 'EA', symbol: 'EA' },
-        { name: 'ExxonMobil', symbol: 'XOM' },
-        { name: 'Goldman Sachs', symbol: 'GS' },
-        { name: 'Google', symbol: 'GOOGL' },
-        { name: 'Home Depot', symbol: 'HD' },
         { name: 'IBM', symbol: 'IBM' },
-        { name: 'Intel', symbol: 'INTC' },
-        { name: 'Johnson & Johnson', symbol: 'JNJ' },
-        { name: 'JPMorgan Chase', symbol: 'JPM' },
-        { name: 'LG', symbol: '066570.KS' },
-        { name: 'Lockheed Martin', symbol: 'LMT' },
-        { name: 'Man United', symbol: 'MANU' },
-        { name: 'Mastercard', symbol: 'MA' },
-        { name: 'Meta', symbol: 'META' },
         { name: 'Microsoft', symbol: 'MSFT' },
-        { name: 'Netflix', symbol: 'NFLX' },
-        { name: 'NIO', symbol: 'NIO' },
         { name: 'Nike', symbol: 'NKE' },
         { name: 'NVIDIA', symbol: 'NVDA' },
-        { name: 'Open AI', symbol: 'Not Listed' }, // Open AI is not a publicly traded company
-        { name: 'Pandora', symbol: 'P' },
-        { name: 'PayPal', symbol: 'PYPL' },
         { name: 'Pfizer', symbol: 'PFE' },
-        { name: 'PepsiCo', symbol: 'PEP' },
-        { name: 'Procter & Gamble', symbol: 'PG' },
         { name: 'Roblox', symbol: 'RBLX' },
-        { name: 'Rolls Royce', symbol: 'RR.L' },
         { name: 'Shell', symbol: 'SHEL' },
         { name: 'Spotify', symbol: 'SPOT' },
         { name: 'Tesla', symbol: 'TSLA' },
-        { name: 'Tesco', symbol: 'TSCO.L' },
-        { name: 'UnitedHealth', symbol: 'UNH' },
-        { name: 'Verizon', symbol: 'VZ' },
         { name: 'Visa', symbol: 'V' },
         { name: 'Walmart', symbol: 'WMT' }
       ],
@@ -92,16 +58,39 @@ export default {
     onAuthStateChanged(auth, async (user) => {
       if (user) {
         this.user = user;
+        await trackUserLogin(user.uid);
         await this.checkAndFetchStockData();
-        this.$router.push({ name: 'StockTradingSelect' });
       } else {
+        if (this.user) {
+          await trackUserLogout(this.user.uid);
+          await stopTrackingUserLogin(this.user.uid);
+        }
         this.user = null;
         this.$router.push({ name: 'StartPage' });
       }
       this.loading = false;
     });
+
+    // Add beforeunload event listener
+    window.addEventListener('beforeunload', this.handleBeforeUnload);
+
+    // Run the conversion rate checker
+    await fetchAndSaveConversionRate();
+  },
+  beforeUnmount() {
+    // Remove beforeunload event listener
+    window.removeEventListener('beforeunload', this.handleBeforeUnload);
   },
   methods: {
+    async handleBeforeUnload() {
+      const auth = getAuth();
+      const user = auth.currentUser;
+      if (user) {
+        await trackUserLogout(user.uid);
+        await stopTrackingUserLogin(user.uid);
+        await signOut(auth);
+      }
+    },
     async checkAndFetchStockData() {
       for (const company of this.companies) {
         const symbol = company.symbol;
@@ -112,7 +101,7 @@ export default {
       }
     },
     async fetchAndSaveStockData(symbol) {
-      // Fetch stock data from API
+      // Fetch stock data from Alpha Vantage API
       const stockData = await getStockData(symbol); 
       // Save to Firestore
       await saveStockDataToFirestore(symbol, stockData);
